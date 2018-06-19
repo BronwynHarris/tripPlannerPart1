9ffd31fcf1a12eb1fc6d3d3002253ef53663a9b3
Alexandra Ash <alexandraleah@gmail.com>
2018-06-19 16:21:42 -0400

    added map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log('hi');
+const mapboxgl = require('mapbox-gl');
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYWxleGFuZHJhbGVhaCIsImEiOiJjamltNDg3OXMwMDZ5M3BwYXB4aGM1MzA0In0.Ct_0GIAkZf1Y1ehRzO6SQQ';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
